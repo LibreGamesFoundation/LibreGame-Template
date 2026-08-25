79325daf38287efa7f4bd21e254960f9d85b6774
Ian Day <ian.day@mailbox.org>
2026-08-25 16:57:18 -0400

    feat: Introduce ComponentBase Node

---
+++ scripts/classes/components/component_base.gd
@@ -0,0 +1,11 @@
+@icon("res://assets/components/ComponentNode.svg")
+class_name ComponentBase
+extends Node
+
+## Base for all Components that exist outside of 2D or 3D Space.
+##
+## Contains the most universal variables for all components.
+##
+
+@export var debug : bool = false
+@export var target : Node
