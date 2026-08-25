e8bfb05e2e2c6d1ddf5e300146653b0d10e58ab0
Ian Day <ian.day@mailbox.org>
2026-08-25 17:53:25 -0400

    feat: Introduce Component2D & Component3D

---
+++ scripts/classes/components/component_2d.gd
@@ -0,0 +1,11 @@
+class_name Component2D
+extends Node2D
+
+## Base for all Components that exist within 2D Space.
+##
+## Contains the most universal variables for all components.
+##
+
+@export var debug : bool = false
+@export var target : Node
+@export var initial_position : Vector2
