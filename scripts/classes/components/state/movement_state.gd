0c25e2b744a70120697f266a1d5528bd1f9e3a90
Ian Day <ian.day@mailbox.org>
2026-09-01 15:59:15 -0400

    feat: Added MovementState

---
+++ scripts/classes/components/state/movement_state.gd
@@ -0,0 +1,15 @@
+class_name MovementState
+extends State
+
+@export var speed : float = 8
+@export var acceleration : float = .5
+@export var deceleration : float = .8
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
