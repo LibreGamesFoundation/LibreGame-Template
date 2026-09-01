403edfbf7bd75ef5787ff474e77ed4f910301ddf
Ian Day <ian.day@mailbox.org>
2026-09-01 16:11:11 -0400

    feat: Introduce Interaction System

---
+++ scripts/classes/components/interaction_area_3d.gd
@@ -0,0 +1,13 @@
+@icon("res://assets/components/InteractionArea3D.svg")
+class_name InteractionArea3D
+extends Area3D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
